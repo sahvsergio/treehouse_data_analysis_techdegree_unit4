d63370817633631b276ad5a1c1c2c60d1f34b009
sahvsergio <sergioandresherrera@gmail.com>
2024-09-22 22:10:47 -0500

    added relationship to the models

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import datetime
-from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Date,ForeignKey
+from sqlalchemy.orm import DeclarativeBase, sessionmaker,relationship
 
 
 
@@ -31,15 +31,32 @@ class Product(Base):
     # product_quantity,
     # product_price,
     # date_updated.
+    #brand id (ForeignKey)
     product_id = Column('Product Id', Integer, primary_key=True)
     product_name = Column('Product Name', String)
     product_quantity = Column('Product Quantity', Integer)
     product_price = Column('Product Price', Integer)
     date_updated = Column('Date Updated', Date, default=datetime.datetime.now)
+    brand_id=Column('brand_id', ForeignKey("Brands.Brand Id"))
 
+    
     def __repr__(self) -> str:
         ...
         return f"'Product Name:{self.product_name},\
             Quantity: {self.product_quantity}, \
             Price: {self.product_price}, \
             Date Updated: {self.date_updated} "
+
+class Brand(Base):
+    __tablename__='Brands'
+    brand_id = Column('Brand Id', Integer, primary_key=True)
+    
+    brand_name = Column('Brand Name', String)
+    product=relationship("Product")
+
+
+    def __repr__(self)->str:
+        pass
+    
+
+        
